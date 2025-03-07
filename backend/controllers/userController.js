04e0fa432836c7735b1a5fe97ba5e0ef8ba83a94
JanhviPatil08 <janhvinpatil@gmail.com>
2025-03-07 20:24:15 +0530

    updated files

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,3 +47,15 @@ export const getFavorites = async (req, res) => {
     res.status(500).json({ message: "Server error: " + error.message });
   }
 };
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};

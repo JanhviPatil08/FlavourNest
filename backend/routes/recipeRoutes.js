7354a74806f792a5dab6d98d87898aa4cd75275d
JanhviPatil08 <janhvinpatil@gmail.com>
2025-03-08 06:41:23 +0530

    updated

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getRecipes, createRecipe } from "../controllers/recipeController.js";
+import { getRecipes, createRecipe,getUserRecipes } from "../controllers/recipeController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();

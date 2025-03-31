f84a8ccda47f14458127d981486486dd0633f7bd
JanhviPatil08 <janhvinpatil@gmail.com>
2025-03-31 15:34:04 +0530

    updated

--- frontend/src/components/SplashScreen.jsx
+++ frontend/src/components/SplashScreen.jsx
@@ -3,7 +3,17 @@
 const SplashScreen = () => {
     return (
         <div className="splash-screen">
-            <img src="/logo-png.png"  />
+           <img 
+  src="logo-png.png" 
+  className="splash-image"
+  style={{
+    maxWidth: "90vw", 
+    maxHeight: "80vh", 
+    width: "auto", 
+    height: "auto"
+  }} 
+/>
+
         </div>
     );
 };
